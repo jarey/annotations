f6bfd793286d593d743c7ecd0cb1896f30dbe922
Alex Cook <alexcook@Alexs-MacBook-Pro.local>
2016-08-19 21:56:38 -0400

    Fix PDF rendering because of bullshit READme

--- src/PDFable.js
+++ src/PDFable.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import PDFJS from 'pdfjs-dist';
 import PDFJSAnnotate from 'pdf-annotate';
+
+const testPDF = '../static/feasibility-study.pdf';
 
 const { UI } = PDFJSAnnotate;
 const RENDER_OPTIONS = {
+  documentId: 'pdfview',
   pdfDocument: null,
   scale: 1,
   rotate: 0
@@ -14,11 +18,29 @@ export class PDFable extends React.Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    PDFJS.getDocument('../static/feasibility-study.pdf').then((pdf) => {
+      pdf.getPage(1).then((page) => {
+        let scale = 1.5;
+        let viewport = page.getViewport(scale);
+        let canvas = document.getElementById('pdfview');
+        let context = canvas.getContext('2d');
+
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        let renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        page.render(renderContext);
+      });
+    });
+  }
+
   render() {
     return (
-      <div>
-        There should be a PDF here
-      </div>
+      <div id="pdfview"></div>
     );
   }
 }
