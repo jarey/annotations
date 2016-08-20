b77b726a5ed5f433d712f01c8463b56f60bbf2a7
Alex Cook <alexcook@Alexs-MacBook-Pro.local>
2016-08-19 20:59:29 -0400

    fix import for pdfable

--- .storybook/components/pdfable.js
+++ .storybook/components/pdfable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
-import PDFable from '../../src/PDFable';
+import { PDFable } from '../../src/PDFable';
 
 storiesOf('PDFable', module)
   .add('Render a PDF', () => (
